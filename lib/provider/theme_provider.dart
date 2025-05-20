67fa05694c0fc6ba0a72601cbbb379fd5a30c5d5
Ismayil <ismayilov_ismayil_97@mail.ru>
2025-05-21 00:12:20 +0400

    add theme to storage

--- lib/provider/theme_provider.dart
+++ lib/provider/theme_provider.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   bool isDarkMode = false;
 
-  void toggleTheme() {
+  ThemeProvider() {
+    _loadTheme(); // при создании провайдера — загрузим сохранённую тему
+  }
+
+  void toggleTheme() async {
     isDarkMode = !isDarkMode;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkMode', isDarkMode);
+  }
+
+  void _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    isDarkMode = prefs.getBool('isDarkMode') ?? false;
     notifyListeners();
   }
 }
